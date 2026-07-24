de4ce6ac749419af4c95f7a211dcdb327e1a5fbc
tang <tangbao510@gmail.com>
2026-07-24 20:44:27 +0800

    Load asset and timeline v2 styles

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
 import './timeline-enhancements.css'
+import './asset-timeline.css'
 
 createApp(App).mount('#app')
