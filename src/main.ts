fd366e06863f39bca780303f84212e3ff0426f0c
tang <tangbao510@gmail.com>
2026-07-25 17:48:26 +0800

    Mount hash-routed application

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import RootApp from './RootApp.vue'
+import { router } from './router'
 import './styles.css'
 import './timeline-enhancements.css'
 import './asset-timeline.css'
@@ -11,4 +12,4 @@ import './capcut-layout.css'
 import './upload-dialog.css'
 import './asset-manager.css'
 
-createApp(App).mount('#app')
+createApp(RootApp).use(router).mount('#app')
