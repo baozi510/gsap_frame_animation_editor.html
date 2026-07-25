81e2ff36750040e64a3cb6029429a6a8c05e5c1d
tang <tangbao510@gmail.com>
2026-07-25 17:47:36 +0800

    Add asset library route page

---
+++ src/pages/AssetLibraryRoute.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import AssetManagerPage from '@/components/AssetManagerPage.vue'
+import SettingsDialog from '@/components/SettingsDialog.vue'
+import UploadDialog from '@/components/UploadDialog.vue'
+import { workspaceTime } from '@/state/workspaceState'
+
+const router = useRouter()
+const uploadOpen = ref(false)
+const settingsOpen = ref(false)
+
+function openUploadSettings() {
+  uploadOpen.value = false
+  settingsOpen.value = true
+}
+</script>
+
+<template>
+  <main class="route-page asset-library-route">
+    <AssetManagerPage
+      :current-time="workspaceTime"
+      @back="router.push('/')"
+      @upload="uploadOpen = true"
+      @settings="settingsOpen = true"
+    />
+    <SettingsDialog :open="settingsOpen" @close="settingsOpen = false" />
+    <UploadDialog :open="uploadOpen" @close="uploadOpen = false" @settings="openUploadSettings" />
+  </main>
+</template>
